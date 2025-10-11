f4593fc250199ad87a75f08d85d9f4a859958ce3
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-10-11 12:30:46 +0530

    add: object type custom and separate flows for coin and dice

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 import logging
 
-from modules.probability import simulate_single_probability, display_distribution_table
+from modules.probability import simulate_probability_of_single_dice, display_distribution_table
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -  %(message)s')
 
@@ -14,7 +14,7 @@ subparsers = parser.add_subparsers(dest='myCommand', required=True)
 
 parser_simulation = subparsers.add_parser('simulation', help='Probability simulation to simulate dice rolls, coin tosses etc')
 parser_simulation.add_argument('-type', '--objectType', help='Type of object to use for simulation', dest='objectType', choices=[
-    'dice', 'coin'
+    'dice', 'coin', 'custom'
 ], default='dice')
 parser_simulation.add_argument('-acceptable', '--acceptableOutcome', default=1, type=int, dest='acceptableOutcome')
 parser_simulation.add_argument('-total', '--totalOutcome', default=6, type=int, dest='totalOutcome')
@@ -28,17 +28,28 @@ parser_analyser.add_argument('-a', '--all', help='Display stock analysis data fr
 args = parser.parse_args()
 
 if args.myCommand == 'simulation':
-    print('Succesfully entered the simulation block')
-    if args.objectType:
-        try:
-            simulate_single_probability(args.acceptableOutcome, args.totalOutcome, args.totalTries)
-        except ValueError as e: 
-            print(f'There has been a value error: {e}')
+    if args.objectType == 'custom':
+        print('The object type is custom')
+        sys.exit(0)
+    else:
+        if args.objectType == 'coin':
+            try:
+                simulate_probability_of_single_dice(args.totalTries, object_type=args.objectType)
+            except ValueError as e: 
+                print(f'There has been a value error: {e}')
+            else:
+                print(f'After {args.totalTries} tries for coin toss, the results are: \n')
+                result = simulate_probability_of_single_dice(args.totalTries, object_type=args.objectType)
+                display_distribution_table(result)
         else:
-            result = simulate_single_probability(args.acceptableOutcome, args.totalOutcome, args.totalTries)
-            print(f'The answer is: {result}')
-
-            display_distribution_table(result)
+            try:
+                simulate_probability_of_single_dice(args.totalTries, object_type=args.objectType)
+            except ValueError as e: 
+                print(f'There has been a value error: {e}')
+            else:
+                print(f'After {args.totalTries} tries of dice roll, the results are: \n')
+                result = simulate_probability_of_single_dice(args.totalTries, object_type=args.objectType)
+                display_distribution_table(result)
     sys.exit(0)
 
 if args.myCommand == 'analyser':
--- src/modules/probability.py
+++ src/modules/probability.py
@@ -3,48 +3,62 @@ import random
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -  %(levelname)s -  %(message)s')
 
-def simulate_single_probability(acceptable_outcome: int, total_possible_outcomes: int, total_tries: int) -> dict:
+def simulate_probability_of_single_dice(total_tries: int, object_type: str = 'dice') -> dict:
 
     """
-    Simulates the probability of the object based on given parameters
+    Simulates the probability of a single dice roll for times provided by the user
 
     Args:
-    acceptable_outcome(int): Total number of outcomes acceptable to the user
-    total_possible_outcomes(int): Total number of outcomes that can occur
     total_tries(int): Number of tries that can be made in the simulation
 
     Returns:
     The probability based on the above args
     """
-
-    if acceptable_outcome <= 0 or total_possible_outcomes <= 0:
-        raise ValueError('Cannot be less than or equal to 0')
-    
-    if total_possible_outcomes < acceptable_outcome:
-        raise ValueError('Possible outcomes cannot be < acceptable outcomes')
     
     if total_tries <= 0:
         raise ValueError('Total tries should be >= 1')
+        
+    if object_type != 'dice' and object_type != 'coin': 
+        raise ValueError('Object type should be dice or coin')
     
+    if object_type == 'dice':
 
-    frequency_storage_dict: dict = {
-        1: [0,0],
-        2: [0,0],
-        3: [0,0],
-        4: [0,0],
-        5: [0,0],
-        6: [0,0]
-    }
-    
+        frequency_storage_dict: dict = {
+            1: [0,0],
+            2: [0,0],
+            3: [0,0],
+            4: [0,0],
+            5: [0,0],
+            6: [0,0]
+        }
+        
 
-    for i in range(total_tries):
-        current_number: int = random.randint(1, 6)
-        frequency_storage_dict[current_number][0] += 1
+        for i in range(total_tries):
+            current_number: int = random.randint(1, 6)
+            frequency_storage_dict[current_number][0] += 1
 
-    for i in frequency_storage_dict.items():
-        if i[1][0] == 0: 
-            continue
-        i[1][1] = i[1][0] / total_possible_outcomes
+        for i in frequency_storage_dict.items():
+            if i[1][0] == 0: 
+                continue
+            i[1][1] = i[1][0] / total_tries
+
+    else:
+
+        frequency_storage_dict: dict = {
+            'Heads': [0,0],
+            'Tails': [0,0]
+        }
+
+        possible_results = ['Heads', 'Tails']
+
+        for i in range(total_tries):
+            current_result: str = random.choice(possible_results)
+            frequency_storage_dict[current_result][0] += 1
+
+        for i in frequency_storage_dict.items():
+            if i[1][0] == 0: 
+                continue
+            i[1][1] = i[1][0] / total_tries
 
     return frequency_storage_dict
 
