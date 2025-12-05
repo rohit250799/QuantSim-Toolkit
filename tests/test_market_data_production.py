e70a33cc2384b294f8e612cffd2a39e2e77f06ad
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-12-05 14:18:17 +0530

    add: unit tests for table creation and data insertion in table in the db

--- tests/test_market_data_production.py
+++ tests/test_market_data_production.py
@@ -11,20 +11,21 @@ import tempfile
 logging.basicConfig(filename='logs/pytest_logs.txt', level=logging.DEBUG,
                     format=' %(asctime)s -  %(levelname)s -  %(message)s')
 
+@pytest.fixture(scope="function")
+def test_db_connection():
+    """
+    Each test gets a fresh transaction and rolls it back.
+    Pure sqlite3 and standard library only.
+    """
+    # Establishing single in memory connection - guarantees isolation from disk DB and other tests
+    conn = sqlite3.connect(':memory:')
+    # Passing the Active connection object to the test function
+    yield conn
+    # close connection after test, destroying the in-memory DB
+    conn.close()
 
 class TestFinancialDataDownloader:
     """Testing all the functions of the financial data downloader class"""
-
-    @pytest.fixture(scope="function")
-    def test_db_path(self):
-        """Used to provide the test database path for the underneath functions"""
-        #option A - for unit tests
-        return ":memory:"
-    
-        #option B - for the integration tests
-        # with tempfile.NamedTemporaryFile(suffix=".sqlite3", delete=False) as tmp:
-        #     yield tmp.name
-        # os.unlink(tmp.name) # Cleans up the file after the test
 
     def test_initialization(self) -> None:
         api_key_obtained = os.environ.get('ALPHA_VANTAGE_API_KEY', 'key not found')
@@ -39,28 +40,39 @@ class TestFinancialDataDownloader:
 
         logging.info('The assertions made on creation of financial data downloader are all correct - the api key and base url are all rightly fetched')
 
-    def test_table_creation(self, test_db_path):
-        """Unit test to test the creation of a table inside the database using a manual query"""
+    def test_table_creation(self, test_db_connection):
+        """Test the creation of a new table in the in-memory database while performing unit tests"""
         table_name = 'my_new_symbol_table'
-        execute_query(test_db_path, f"CREATE TABLE {table_name} (id INTEGER PRIMARY KEY, symbol TEXT);")
-        created_tables = execute_query(test_db_path, "SELECT name FROM sqlite_master WHERE type='table';")
-    
-        assert (table_name,) in created_tables
+        execute_query(test_db_connection, f"CREATE TABLE {table_name} (id INTEGER PRIMARY KEY, symbol TEXT);")
+        created_tables = execute_query(test_db_connection, "SELECT name FROM sqlite_master WHERE type='table';")
+        logging.debug('The newly created tables from test table creation function are: %s', execute_query(test_db_connection, "SELECT name FROM sqlite_master WHERE type='table';"))
+        assert('my_new_symbol_table',) in created_tables
 
-    @pytest.fixture
-    def existing_symbol(self):
-        return "TCS"
+    def test_data_insertion(self, test_db_connection):
+        """Tests the insertion of data in the newly created table""" 
+        table_name = 'my_new_symbol_table'
+        execute_query(test_db_connection, f"CREATE TABLE {table_name} (id INTEGER PRIMARY KEY, symbol TEXT);")
+        execute_query(test_db_connection, f"insert into {table_name}(symbol) values(?)", ('TCS',))
+        logging.info('The symbol to be inserted is: TCS')
+        insertion_query_fetch_result = execute_query(test_db_connection, "select * from my_new_symbol_table where symbol = ?", ('TCS', ))
+        logging.debug('The insertion query fetch result is: %s', insertion_query_fetch_result)
+
+
     
-    @pytest.fixture
-    def missing_symbol(self):
-        return 'B'
+    # @pytest.fixture
+    # def existing_symbol(self):
+    #     return "TCS"
     
-    def test_insert_order(self, test_db_path):
-        execute_query(test_db_path, symbol_table_creation_query)
-        #created_tables_list = execute_query(test_db_path, list_tables)
-        created_tables_list = list_tables(test_db_path)
-        #symbols_table_fetching_data = execute_query(test_db_path, "select * from symbols;")
-        logging.debug('The created tables are: %s', created_tables_list)
-        return
+    # @pytest.fixture
+    # def missing_symbol(self):
+    #     return 'B'
+    
+    # def test_insert_order(self, test_db_path):
+    #     execute_query(test_db_path, symbol_table_creation_query)
+    #     #created_tables_list = execute_query(test_db_path, list_tables)
+    #     created_tables_list = list_tables(test_db_path)
+    #     #symbols_table_fetching_data = execute_query(test_db_path, "select * from symbols;")
+    #     logging.debug('The created tables are: %s', created_tables_list)
+    #     return
 
     
