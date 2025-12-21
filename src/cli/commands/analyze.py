bf4dc4ed78176d7fd0cf92fd6fb02add94827f35
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-12-21 22:15:57 +0530

    rm: unnecessary code

--- src/cli/commands/analyze.py
+++ src/cli/commands/analyze.py
@@ -1,5 +1,4 @@
 from src.modules.analytics.returns_analyzer import (
-    #calculate_daily_returns_from_hardcoded_list,
     summarize_returns,
     read_all_csv_data, 
     #calculate_daily_portfolio_returns,
@@ -27,15 +26,6 @@ def run_analyze(args: argparse.Namespace) -> None:
     file_path = Path(stock_csv_file_path) / stock_csv_file_name
     stock_csv_file_dataframe = read_all_csv_data(stock_symbol)
     logging.debug('The stock symbol is: %s', stock_symbol)
-
-    # if not file_path.is_file():
-    #     logging.info('Stock csv file not found in path: %s. Trying analysis from hardcoded list.', stock_csv_file_path)
-    #     stock_closing_price_for_testing: List[int] = [100, 102, 101, 105, 107]
-    #     stock_closing_price_for_testing_pandas_series: pd.Series = pd.Series(stock_closing_price_for_testing)
-    #     result = calculate_daily_returns_from_hardcoded_list(stock_closing_price_for_testing_pandas_series)
-    #     print(f'The result is: {result}')
-    #     print(summarize_returns(stock_closing_prices_dataframe=stock_closing_price_for_testing_pandas_series, stock_name=stock_symbol))
-    #     return
     
     logging.debug('Stock CSV File found in path. Starting the analysis')
     file_data = read_all_csv_data(stock_symbol=stock_symbol)
