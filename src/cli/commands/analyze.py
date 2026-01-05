15f0c165e6dbe9256c0a3e6b016d79810ad29c43
Rohit Sarkar <rohitsarkar034@gmail.com>
2026-01-05 09:59:17 +0530

    splitting: annualized volatility into ticker and benchmark annualized volatility, and returning analysis report in running the analyze command

--- src/cli/commands/analyze.py
+++ src/cli/commands/analyze.py
@@ -1,6 +1,6 @@
 from src.flow_controller import FlowController
 
-from typing import List
+from typing import List, Dict, Any
 import logging
 import argparse
 
@@ -10,7 +10,11 @@ logger = logging.getLogger("cli")
 # the CSV file all at once. Later, will create another feature which will use generator to load
 # CSV file in chunks if the dataset is large 
 
-def run_analyze(args: argparse.Namespace, flow_controller: FlowController | None) -> None:
+def run_analyze(args: argparse.Namespace, flow_controller: FlowController | None) -> Dict[str, Any]:
+    """
+    Runs the Analyze command for the project
+    Returns - the analysis report 
+    """
     logging.debug('Entered the run analyze function block!')
     if not flow_controller:
         raise RuntimeError('Flow Conytroller is not properly initialized. Complete Initialization first ')
@@ -18,8 +22,9 @@ def run_analyze(args: argparse.Namespace, flow_controller: FlowController | None
     input_ticker = args.ticker
     input_benchmark = args.benchmark if args.benchmark else 'NIFTY50_id.csv'
 
-    flow_controller.dispatch_analysis_request(input_ticker, input_benchmark, args.startDate, args.endDate)
+    analysis_report = flow_controller.dispatch_analysis_request(input_ticker, input_benchmark, args.startDate, args.endDate)
+    logger.debug('The analysis report is: %s', analysis_report)
     
-    return
+    return analysis_report
 
 
