605ba0b122f8b2696c71d21abef25e7953cff787
Rohit Sarkar <rohitsarkar034@gmail.com>
2026-02-11 09:46:03 +0530

    modify: PriceLevel constructor implementation and add: addOrder function definition

--- cpp/src/lob/Price_level.cpp
+++ cpp/src/lob/Price_level.cpp
@@ -1,21 +1,27 @@
 #include <pybind11/pybind11.h>
+#include <csignal>
 #include <quantsim/lob/Price_level.hpp>
 #include <queue>
+#include <stdexcept>
+#include <iostream>
 
 namespace py = pybind11;
 
 PriceLevel::PriceLevel(
-    float price,
-    std::queue<Order> fifo_order_container_by_price_level,
-    int aggregateQuantity
+    float price
     )
         : price(price)
-        , fifo_order_container_by_price_level(fifo_order_container_by_price_level)
-        , aggregateQuantity(aggregateQuantity)
     {}
     
-PriceLevel::addOrder(Order order) {
+void PriceLevel::addOrder(Order order) {
     // adds an order to the price level
-    fifo_order_container_by_price_level.push(order);
+    // only valid orders are to be added to the Price Level queue
+    if (order.validate() != OrderValidationError::NONE) {
+        throw std::invalid_argument("Order is not valid. Check the order again!");
+    }
+    std::cout << "Adding order to the queue \n";
+    fifo_order_container_queue_by_price_level.push(order);
+    std::cout << "Order has been successfully added to the queue \n";
+    aggregateQuantity = aggregateQuantity + order.getOriginalQuantity();
 }
 
